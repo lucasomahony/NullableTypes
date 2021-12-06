2343e13e6c08aaf208a87d97154b84f77464dbc3
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 08:59:16 +0000

    greet function added

---
+++ src/NullableTypes.kt
@@ -0,0 +1,11 @@
+fun greet(name: String) : String {
+    return "hello $name"
+}
+
+fun main(args: Array<String>){
+    println("Please type your name")
+    val name = readLine()
+
+    val greeting = greet(name)
+    println(greeting)
+}
