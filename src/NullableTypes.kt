2b85322251168ce1efcbcdde7dc48219c75929ee
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-06 11:42:30 +0000

    task 3 solution fixed

--- src/NullableTypes.kt
+++ src/NullableTypes.kt
@@ -1,6 +1,10 @@
 fun greet(name: String?) : String {
-    val x = name ?: "hello world"
-    return x
+
+    val x = name ?: -1
+    if(x != -1)
+        return "hello $name"
+    else
+        return "hello world"
 }
 
 fun main(args: Array<String>){
